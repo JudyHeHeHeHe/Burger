7b0361f7137db7b55ceee6914ffb5678de336bed
Judy He <jhe@sankynet.com>
2019-12-30 13:20:41 -0500

    initiate the burger app

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import Layout from "./hoc/Layout/Layout";
+import BurderBuilder from './containers/BurgerBuilder/BurgerBuilder';
+
+
+class App extends Component {
+  render(){
+    return (
+      <div>
+        <Layout>
+          <BurderBuilder/>
+        </Layout>
+      </div>
+    )
+  }
+}
+
+
+export default App;
---
+++ src/hoc/Layout/Layout.js
@@ -0,0 +1,38 @@
+import React, {Component} from 'react';
+import classes from './Layout.css';
+import Aux from '../Aux/Aux';
+import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
+import SideDrawer from '../../components/Navigation/SideDrawer/SideDrawer'
+
+
+class Layout extends Component {
+	state ={
+		showSideDrawer: false
+	}
+
+	SideDrawerClosedHandler = () => {
+		this.setState({showSideDrawer:false})
+	}
+
+	SideDrawerToggleHandler = () => {
+    this.setState((prevState) => {
+    	return {
+    		showSideDrawer:!this.state.showSideDrawer
+    	}
+    })
+	}
+
+	render (){
+		return (
+			<Aux>
+			  <Toolbar DrawerToggleClicked = {this.SideDrawerToggleHandler}/>
+			  <SideDrawer open={this.state.showSideDrawer} closed={this.SideDrawerClosedHandler}/>
+				<main className={classes.Content}>
+					{this.props.children}
+				</main>
+			</Aux>
+		)
+	}
+}
+
+export default Layout;
